bd6adcdcf448078bad048ed743f4d5210d6a62e8
Unknown <filip98@spray.se>
2020-02-14 09:28:36 +0100

    main restrukter

--- app/src/main/java/com/example/inventeringsapp/main/MainActivity.kt
+++ app/src/main/java/com/example/inventeringsapp/main/MainActivity.kt
@@ -1,12 +1,21 @@
-package com.example.inventeringsapp
+package com.example.inventeringsapp.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.inventeringsapp.OnStart
+import com.example.inventeringsapp.R
+import javax.inject.Inject
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        OnStart.applicationComponent.inject(this)
     }
 }
