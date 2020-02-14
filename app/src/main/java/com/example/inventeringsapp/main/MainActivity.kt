fe0d42637793c1e29c238264abd879474c45c727
Unknown <filip98@spray.se>
2020-02-14 10:07:39 +0100

    created a login page and move login to it

--- app/src/main/java/com/example/inventeringsapp/main/MainActivity.kt
+++ app/src/main/java/com/example/inventeringsapp/main/MainActivity.kt
@@ -3,26 +3,18 @@ package com.example.inventeringsapp.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import com.example.inventeringsapp.OnStart
 import com.example.inventeringsapp.R
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.firebase.auth.FirebaseAuth
+import com.example.inventeringsapp.login.LoginActivity
+import com.example.inventeringsapp.repository.DB
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mGoogleSignInClient : GoogleSignInClient
-    private lateinit var auth: FirebaseAuth
 
     @Inject
     lateinit var viewModel: MainViewModel
@@ -32,88 +24,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         OnStart.applicationComponent.inject(this)
 
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
-
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
-        auth = FirebaseAuth.getInstance()
-
-        sign_in_button.setOnClickListener {
-            signIn()
-        }
         btn_logout.setOnClickListener {
             signOut()
         }
     }
 
-    private fun signIn() {
-        val signInIntent = mGoogleSignInClient.signInIntent
-        startActivityForResult(signInIntent, 123)
-    }
-
-    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == 123) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                val account = task.getResult(ApiException::class.java)
-                firebaseAuthWithGoogle(account!!)
-            } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
-                Log.w(TAG, "Google sign in failed", e)
-                // ...
-            }
-        }
-    }
-
-    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
-        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.id!!)
-
-        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithCredential:success")
-                    val user = auth.currentUser
-                    updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithCredential:failure", task.exception)
-                    updateUI(null)
-                }
-
-            }
-    }
-
-
     public override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
+        val currentUser = DB.auth.currentUser
         updateUI(currentUser)
     }
 
     private fun updateUI(user: FirebaseUser?) {
         if (user != null) {
-            sign_in_button.visibility = View.GONE
             btn_logout.visibility = View.VISIBLE
         } else {
-            sign_in_button.visibility = View.VISIBLE
-            btn_logout.visibility = View.GONE
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 
     private fun signOut() {
-        auth.signOut()
-
-        mGoogleSignInClient.signOut().addOnCompleteListener(this) {
+        DB.auth.signOut()
+        DB.mGoogleSignInClient(this).signOut().addOnCompleteListener(this) {
             updateUI(null)
         }
     }
