101cc06fdbe9e0fc5f4e6c7667e058256b1daa1a
Unknown <filip98@spray.se>
2020-03-20 14:27:21 +0100

    add refactoring

--- app/src/main/java/com/example/inventeringsapp/sheet/sheetfragments/AddItemFragment.kt
+++ app/src/main/java/com/example/inventeringsapp/sheet/sheetfragments/AddItemFragment.kt
@@ -3,6 +3,7 @@ package com.example.inventeringsapp.sheet.sheetfragments
 import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +12,7 @@ import androidx.fragment.app.Fragment
 import com.example.inventeringsapp.R
 import com.example.inventeringsapp.repository.DB.Companion.mService
 import com.example.inventeringsapp.sheet.SheetActivity
+import com.google.android.material.tabs.TabLayout
 import com.google.api.services.sheets.v4.SheetsRequestInitializer
 import com.google.api.services.sheets.v4.model.AppendValuesResponse
 import com.google.api.services.sheets.v4.model.ValueRange
@@ -19,7 +21,7 @@ import java.io.IOException
 import java.util.*
 import kotlin.random.Random
 
-
+private const val TAG = "AddItemFragment"
 class AddItemFragment : Fragment() {
 
     lateinit var textView: TextView
@@ -42,33 +44,55 @@ class AddItemFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         btn_addItem.setOnClickListener {
+
+            var q = false
+            var c = false
+            var n = false
             if(editText_addItemQuantity.text.toString() != "" || editText_addItemQuantity.text.toString().length > 9 && editText_addItemQuantity.text.toString().length < 10){
                 quantity = editText_addItemQuantity.text.toString().toDouble()
+                q = true
+            }else{
+                editText_addItemQuantity.setHintTextColor(Color.RED)
             }
 
             if(editText_addItemCost.text.toString() != "" || editText_addItemCost.text.toString().length > 9 && editText_addItemCost.text.toString().length < 10){
                 cost = editText_addItemCost.text.toString().toDouble()
+                c = true
+            }else{
+                editText_addItemCost.setHintTextColor(Color.RED)
             }
 
             if (editText_addItemName.text.toString() == "") {
                 editText_addItemName.setHint("Fill in Name")
                 editText_addItemName.setHintTextColor(Color.RED)
+            }else{
+                n = true
+                name = editText_addItemName.text.toString()
             }
-            else{
+
+            if (q && c && n){
                 editText_addItemName.getText().clear()
                 editText_addItemQuantity.getText().clear()
                 editText_addItemCost.getText().clear()
+                editText_addItemQuantity.setHintTextColor(Color.GRAY)
+                editText_addItemCost.setHintTextColor(Color.GRAY)
+                editText_addItemName.setHintTextColor(Color.GRAY)
+                Log.d(TAG,name + "tacken")
                 addItem(id,name,barcode,quantity,cost,valueprice)
                 Handler().postDelayed({
                     (activity as SheetActivity?)?.printSheet()
                 },1000)
+            }else{
+                Log.d(TAG,n.toString()+"||"+q.toString()+"||"+c.toString())
             }
+
         }
     }
 
     companion object{
         var id :String = ""
         fun addItem(id:String, name:String, barcode:String, quantity:Double,cost:Double,valueprice:Double  ) {
+            Log.d(TAG,name + "compani object")
             this.id = id
             this.id = Random.nextInt(100000000,999999999).toString()
             val appendBody =
