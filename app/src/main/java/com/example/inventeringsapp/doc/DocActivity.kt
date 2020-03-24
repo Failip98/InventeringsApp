eae6e3d05e301b35b6c2641b5e4fd446dcb91bd1
Unknown <filip98@spray.se>
2020-03-24 11:11:14 +0100

    Add documentation

--- app/src/main/java/com/example/inventeringsapp/doc/DocActivity.kt
+++ app/src/main/java/com/example/inventeringsapp/doc/DocActivity.kt
@@ -14,6 +14,11 @@ class DocActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doc)
+        textView_doc.text = "-Create a google sheet and share it so that every one white the link can edit the file." +
+                "\n"+"-Press get shareable link and the part between /d/ and /edit?usp=sharing is sheet id excluding this to parts."+
+                "\n"+"-Page 1 is the page name if you don´t rename it or want to get a other sheet page."+
+                "\n"+"-Name cell A1 to F6 id, name, barcode, quantity, cost, value."+
+                "\n"+"-Recommendation don't use column A to F more than to the list."
         btn_doc_back.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
